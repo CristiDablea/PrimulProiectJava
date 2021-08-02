feec7269f5e389dec1271e0e4359af8a309aceb1
CristiDablea <cristidablea@yahoo.com>
2021-08-02 10:45:27 +0300

    PrimulProiectJava

---
+++ src/PrimaMeaClasa.java
@@ -0,0 +1,20 @@
+public class PrimaMeaClasa {
+
+    public static void main(String[] args) {
+
+        System.out.println("Salutare! Învăț Java! Aceasta este prima instrucțiune.");
+        System.out.print("Aceasta este");
+        System.out.println(" a doua instrucțiune.");
+
+        System.out.println("Tipuri de date:");
+        System.out.println(4);
+        System.out.println(3.67);
+        System.out.println(5.89f);
+        System.out.println(99999999999L);
+        System.out.println(false);
+        System.out.println(true);
+
+
+    }
+
+}
