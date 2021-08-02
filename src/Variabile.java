9806effafafcce667d242438961880a40b2b73ce
CristiDablea <cristidablea@yahoo.com>
2021-08-02 11:19:48 +0300

    Adaug fisierul cu variabile

---
+++ src/Variabile.java
@@ -0,0 +1,26 @@
+public class Variabile {
+
+    public static void main(String[] args) {
+
+        String nume = "Cristi";
+        System.out.println("Numele meu este " + nume);
+
+        //Îmi place Java!
+
+        String textulMeu = "Îmi place Java!";
+        System.out.println(textulMeu);
+
+        int numarulMeuIntreg = 100;
+        double numarulMeuDouble = 3.14;
+        float numarulMeuFloat = 4.56f;
+        char litera = 'A';
+        boolean valoare = true;
+        System.out.println(numarulMeuIntreg);
+        System.out.println(numarulMeuDouble);
+        System.out.println(numarulMeuFloat);
+        System.out.println(litera);
+        System.out.println(valoare);
+
+    }
+
+}
